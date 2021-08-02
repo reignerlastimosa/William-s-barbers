37e175233f57fc7d17eda329cef725d874bce2d8
Reigner Lastimosa <lastimosareignerc@gmail.com>
2021-08-01 22:10:48 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+var mobile =document.getElementById("mobile-navigation");
+
+
+document.getElementById("menu").addEventListener("click", ()=>{
+
+    if(mobile.style.display=="none"){
+       
+        mobile.style.display ="block";
+
+    }
+
+    else{
+        mobile.style.display="none";
+        
+        
+    }
+
+    
+
+
+})
+
+  
